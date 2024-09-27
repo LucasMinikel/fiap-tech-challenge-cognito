9710144de4530f5b7953135066f67dda7e75660d
LucasMinikel <lucasminikel.m@gmail.com>
2024-09-27 17:24:10 -0300

    ajustando outputs

--- modules/api_gateway/outputs.tf
+++ modules/api_gateway/outputs.tf
@@ -1,3 +1,3 @@
 output "invoke_url" {
-  value = aws_api_gateway_deployment.cpf_auth_deployment.invoke_url
+  value = "${aws_api_gateway_deployment.cpf_auth_deployment.invoke_url}/auth"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "api_gateway_invoke_url" {
+output "api_url" {
   value = module.api_gateway.invoke_url
-}
-
-output "cognito_user_pool_id" {
-  value = module.cognito.user_pool_id
 }
